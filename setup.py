46e8535e1e22ba3963f638af836596ad6d5c9bc9
Berker Peksag <bpeksag@holvi.com>
2019-05-26 01:59:18 +0300

    Complete setup.py to make installable

--- setup.py
+++ setup.py
@@ -1,16 +1,28 @@
+import io
+import re
+
 from setuptools import setup
+
+with io.open('flake8_holvi.py') as f:
+    version = re.search(
+        r'^__version__ = [\'"]([^\'"]*)[\'"]',
+        f.read(),
+        re.M,
+    ).group(1)
+
+with io.open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='flake8-holvi',
-    # TODO: Get it from flake8_holvi.py.
-    version='0.1-dev',
-    description='',
-    long_description='',
+    version=version,
+    description='flake8 plugin to help writing Pythonic code at Holvi',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Berker Peksag',
     author_email='bpeksag@holvi.com',
-    license='MIT',
+    url='https://github.com/holviberker/flake8-holvi',
     py_modules=['flake8_holvi'],
-    zip_safe=False,
     entry_points={
         'flake8.extension': [
             'HLV = flake8_holvi:HolviChecker',
