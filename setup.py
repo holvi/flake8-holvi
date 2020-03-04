a1accc54aef47978933f7189b8d1ef5b3c0f6731
Money Tree <github-ci@holvi.com>
2020-03-04 14:50:13 +0000

    Bump version: 0.3.1 → 0.3.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with io.open('README.md', encoding='utf-8') as f:
 
 setup(
     name='flake8-holvi',
-    version='0.3.1',
+    version='0.3.2',
     description='flake8 plugin to help writing Pythonic code at Holvi',
     long_description=long_description,
     long_description_content_type='text/markdown',
